e5c66170bfc4521d63cf991e86935e5e40b48c20
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-07-03 14:04:19 +0530

    adding login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,8 +36,14 @@ const App = () => {
           <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
         )} />
 
-        <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to='/' />} />
-        <Route path="/signup" element={!isAuthenticated ? <SignUp /> : <Navigate to='/' />} />
+        <Route path="/login" element={
+          !isAuthenticated ? <LoginPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+          } />
+
+        <Route path="/signup" element={
+          !isAuthenticated ? <SignUp /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+          } />
+          
         <Route path="/notifications" element={isAuthenticated ? <NotificationsPage /> : <Navigate to='/login' />} />
         <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to='/login' />} />
         <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to='/login' />} />
---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login } from "../lib/api.js"; // Adjust the import path as necessary
+
+const useLogin = () => {
+
+    const queryClient = useQueryClient();
+
+    const { mutate, isPending, error } = useMutation({
+        mutationFn: login,
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['authUser'] }),
+    });
+
+    return { error, isPending, loginMutation: mutate };
+}
+
+export default useLogin;
